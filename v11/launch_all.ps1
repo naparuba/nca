f91420e8869230addbb1e30940341a5f614e2843
naparuba <naparuba@gmail.com>
2025-10-28 08:04:55 +0100

    11: massive launch

--- v11/launch_all.ps1
+++ v11/launch_all.ps1
@@ -2,92 +2,76 @@
 ############ 1 LAYER ############
 # 1-32
 python .\main.py --skip-if-already --n-layers 1 --hidden-size 32 --epochs 150
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 32 --epochs 100
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 32 --epochs 300
 python .\main.py --skip-if-already --n-layers 1 --hidden-size 32 --epochs 600
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 32 --epochs 500
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 32 --epochs 1000
+
 
 # 1-64
-python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 50
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 100
-python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 200
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 500
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 1000
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 150
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 300
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 64 --epochs 600
+
 
 # 1-128
-python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 50
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 100
-python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 200
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 500
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 1000
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 150
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 300
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 128 --epochs 600
+
 
 # 1-256
-python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 50
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 100
-python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 200
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 500
-#python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 1000
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 150
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 300
+python .\main.py --skip-if-already --n-layers 1 --hidden-size 256 --epochs 600
+
 
 
 ############ 2 LAYER ############
 
 # 2-32
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 50
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 100
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 200
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 500
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 1000
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 150
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 300
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 32 --epochs 600
+
 
 # 2-64
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 50
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 100
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 200
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 500
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 1000
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 150
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 300
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 64 --epochs 600
+
 
 # 2-128
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 50
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 100
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 200
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 500
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 1000
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 150
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 300
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 128 --epochs 600
+
 
 # 2-256
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 50
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 100
-python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 200
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 500
-#python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 1000
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 150
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 300
+python .\main.py --skip-if-already --n-layers 2 --hidden-size 256 --epochs 600
 
 
 ############ 3 LAYER ############
 # 3-32
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 50
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 100
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 200
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 500
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 1000
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 150
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 300
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 32 --epochs 600
+
 
 # 3-64
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 50
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 100
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 200
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 500
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 1000
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 150
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 300
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 64 --epochs 600
 
 # 3-128
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 50
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 100
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 200
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 500
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 1000
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 150
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 300
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 128 --epochs 600
 
 # 3-256
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 50
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 100
-python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 200
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 500
-#python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 1000
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 150
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 300
+python .\main.py --skip-if-already --n-layers 3 --hidden-size 256 --epochs 600
 
 
 
